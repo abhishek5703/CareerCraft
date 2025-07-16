41fd8cad3db7e8a7bb3533abaec3f72639224eba
Abhishek Kumar <abhishek2018kumar12@gmail.com>
2025-07-17 00:53:07 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://career-craft-two.vercel.app/"],
+    origin: ["http://localhost:5173", "https://career-craft-two.vercel.app"],
     credentials: true,
   })
 );
