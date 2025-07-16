c6daa3f03aaa9aad28376a416bcb6619880c01d2
Abhishek Kumar <abhishek2018kumar12@gmail.com>
2025-07-16 22:22:57 +0530

    added backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL ,
 });
 
 api.interceptors.request.use(
